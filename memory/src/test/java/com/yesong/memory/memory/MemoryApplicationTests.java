fb06c768289fbdf3792c7e656c4feb7c11a06dca
yesong <18515889655@163.com>
2019-11-26 14:12:22 +0800

    项目创建

---
+++ memory/src/test/java/com/yesong/memory/memory/MemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yesong.memory.memory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
