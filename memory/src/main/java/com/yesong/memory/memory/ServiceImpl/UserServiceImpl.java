43670804e33354cfb886ab89358eb3cd050ac253
yesong <18515889655@163.com>
2019-12-30 21:05:10 +0800

    增加日志

--- memory/src/main/java/com/yesong/memory/memory/ServiceImpl/UserServiceImpl.java
+++ memory/src/main/java/com/yesong/memory/memory/ServiceImpl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.yesong.memory.memory.ServiceImpl;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.aliyun.oss.common.utils.DateUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.yesong.memory.memory.entity.User;
@@ -10,6 +11,8 @@ import com.yesong.memory.memory.response.CommonResponse;
 import com.yesong.memory.memory.service.UserService;
 import com.yesong.memory.memory.util.RedisKey;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -21,6 +24,7 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserMapper userMapper;
     @Autowired
@@ -51,12 +55,14 @@ public class UserServiceImpl implements UserService {
     @Override
     public CommonResponse login(UserRequest userRequest) {
         List<User> users = userMapper.selectList(new QueryWrapper<User>().lambda().eq(User::getAccount, userRequest.getAccount()));
+        log.info("查询结果{}", JSONObject.toJSONString(users));
         if(users != null){
             User user = users.get(0);
             if(user.getPassword().equals(DigestUtils.md5Hex(userRequest.getPassword()))){
                 String userToken = RedisKey.getUserToken(user.getAccount());
                 String token = DigestUtils.md5Hex(System.currentTimeMillis()+user.getAccount()+user.getPassword());
                 redisTemplate.opsForValue().set(userToken,token,90, TimeUnit.DAYS);
+                log.info("设置缓存成功");
                 return CommonResponse.builder().success(true).message("登录成功").body(token).build();
             }else{
                 return CommonResponse.builder().success(false).message("账号密码错误").build();
